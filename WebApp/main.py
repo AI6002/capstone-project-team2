1ef33a9727c12bdd4ef33ca205274e5746ec8e69
“Roldan340” <mba@Roldans-MacBook-Pro.local>
2023-11-08 14:28:52 -0330

    implemented multi user sessions #25

--- WebApp/main.py
+++ WebApp/main.py
@@ -1,58 +1,103 @@
-from flask import render_template, request
-from flask_login import LoginManager
-from web import create_app, db
-from web.models import User, Role
-from web.auth import auth
-from web.views import views  # Importing views blueprint
-import os
+from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, login_user, login_required, logout_user, current_user, UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 
-# Get the current working directory
-current_dir = os.getcwd()
+# Initialize the Flask application
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+app.config['SECRET_KEY'] = 'Team2'
+db = SQLAlchemy(app)
 
-# Define the template and static directories by appending to the current directory
-template_dir = os.path.join(current_dir, 'templates')
-static_dir = os.path.join(current_dir, 'static')
-
-app = create_app(template_dir, static_dir)  # creating an instance of your application
-
-app.secret_key = os.environ.get('SECRET_KEY', 'Team2')  # Using a fallback value if the environment variable is not found
-
+# Initialize Flask-Login
 login_manager = LoginManager()
 login_manager.login_view = 'login'
 login_manager.init_app(app)
 
-app.register_blueprint(auth)
-app.register_blueprint(views)  # Register the views blueprint here
+# Define the User model
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100))
 
+# Define the user_loader callback
 @login_manager.user_loader
-def load_user(user_id):  # Callback to reload the user object
+def load_user(user_id):
     return User.query.get(int(user_id))
 
-@app.route('/', methods=['GET', 'POST'])
+# Index route, redirect based on authentication
+@app.route('/')
 def index():
-    answer = None
-    if request.method == 'POST':
-        image = request.files['image']
-        question = request.form['question']
-        answer = "This is a dummy answer for the question: " + question
-    return render_template('home.html', answer=answer)  # Using home.html from templates folder
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    return redirect(url_for('login'))
 
+# Login route and view
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        user = User.query.filter_by(username=username).first()
+
+        if user and check_password_hash(user.password, password):
+            login_user(user)
+            return redirect(url_for('home'))
+        else:
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+
+    return render_template('login.html')
+
+# Signup route and view
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+
+    if request.method == 'POST':
+        try:
+            username = request.form.get('username')
+            password = request.form.get('password')
+            hashed_password = generate_password_hash(password, method='sha256')
+
+            new_user = User(username=username, password=hashed_password)
+            db.session.add(new_user)
+            db.session.commit()
+
+            return redirect(url_for('login'))
+        except Exception as e:
+            # If there is any error, flash a message to the user
+            flash(str(e))
+            
+    return render_template('signup.html')
+
+# Logout route
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+# Authenticated home view
+@app.route('/home')
+@login_required
+def home():
+    return render_template('home.html')
+
+# Error handler
 @app.errorhandler(500)
 def internal_server_error(e):
     return render_template('error.html', error=str(e)), 500
 
-def insert_roles():
-    roles = ['Guest', 'User', 'Admin']
-    for role in roles:
-        existing_role = Role.query.filter_by(name=role).first()
-        if not existing_role:
-            role_to_insert = Role(name=role)
-            db.session.add(role_to_insert)
-    db.session.commit()
+# Command to create the database tables
+@app.cli.command('create-db')
+def create_db():
+    db.create_all()
+    print("Database tables created.")
 
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()  # Ensure tables are created here first
-        insert_roles()  # Then insert roles
     app.run(debug=True)
-
