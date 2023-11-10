49e29e5ba757915df6959daba81a8a22f523f9ef
Thevananthan Thevarasa <thevananthan.thevarasa@rinstrum.com>
2023-11-10 15:21:02 -0330

    ripoff main.py into modules

--- WebApp/main.py
+++ WebApp/main.py
@@ -1,104 +1,22 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager, login_user, login_required, logout_user, current_user, UserMixin
-from werkzeug.security import generate_password_hash, check_password_hash
+from flask import Flask
+from extensions import db, login_manager
+from models import User
 
 # Initialize the Flask application
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
-app.config['SECRET_KEY'] = 'Team2'
-db = SQLAlchemy(app)
+app.config['SECRET_KEY'] = 'team2vqa'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///vqa_users.db'  # SQLite database file
 
-# Initialize Flask-Login
-login_manager = LoginManager()
-login_manager.login_view = 'login'
+db.init_app(app)
 login_manager.init_app(app)
 
-# Define the User model
-class User(UserMixin, db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(100), unique=True)
-    password = db.Column(db.String(100))
+login_manager.login_view = 'login'
 
-# Define the user_loader callback
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
 
-# Index route, redirect based on authentication
-@app.route('/')
-def index():
-    if current_user.is_authenticated:
-        return redirect(url_for('home'))
-    return redirect(url_for('login'))
-
-# Login route and view
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if current_user.is_authenticated:
-        return redirect(url_for('home'))
-
-    if request.method == 'POST':
-        username = request.form.get('username')
-        password = request.form.get('password')
-        user = User.query.filter_by(username=username).first()
-
-        if user and check_password_hash(user.password, password):
-            login_user(user)
-            return redirect(url_for('home'))
-        else:
-            flash('Invalid username or password')
-            return redirect(url_for('login'))
-
-    return render_template('login.html')
-
-# Signup route and view
-@app.route('/signup', methods=['GET', 'POST'])
-def signup():
-    if current_user.is_authenticated:
-        return redirect(url_for('home'))
-
-    if request.method == 'POST':
-        try:
-            username = request.form.get('username')
-            password = request.form.get('password')
-            hashed_password = generate_password_hash(password, method='sha256')
-
-            new_user = User(username=username, password=hashed_password)
-            db.session.add(new_user)
-            db.session.commit()
-
-            return redirect(url_for('login'))
-        except Exception as e:
-            # If there is any error, flash a message to the user
-            flash(str(e))
-            
-    return render_template('signup.html')
-
-# Logout route
-@app.route('/logout')
-@login_required
-def logout():
-    logout_user()
-    return redirect(url_for('index'))
-
-# Authenticated home view
-@app.route('/home')
-@login_required
-def home():
-    username = current_user.username
-    return render_template('home.html', username=username)
-
-# Error handler
-@app.errorhandler(500)
-def internal_server_error(e):
-    return render_template('error.html', error=str(e)), 500
-
-# Command to create the database tables
-@app.cli.command('create-db')
-def create_db():
-    db.create_all()
-    print("Database tables created.")
+from routes import *
 
 if __name__ == '__main__':
     app.run(debug=True)
