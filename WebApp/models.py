faa2f24a27b60ff14ea5b93a953f9cd5ae0334aa
Thevananthan Thevarasa <thevananthan.thevarasa@rinstrum.com>
2023-11-10 15:24:25 -0330

    split the models and db initialization into separate modules

---
+++ WebApp/models.py
@@ -0,0 +1,8 @@
+from extensions import db
+from flask_login import UserMixin
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), unique=True, nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
