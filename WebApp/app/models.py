df922410b3067c3477e0608adb261e434bea636d
Thevananthan Thevarasa <thevananthan.thevarasa@rinstrum.com>
2023-11-24 15:25:38 -0330

    use simple pass encryption #37

--- WebApp/app/models.py
+++ WebApp/app/models.py
@@ -1,6 +1,24 @@
 from .extensions import db
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
+import bcrypt
+
+def hash_password(password):
+    # Convert the password to bytes, if it's not already in bytes
+    password_bytes = password.encode('utf-8')
+
+    # Generate a salt and hash the password
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(password_bytes, salt)
+
+    return hashed_password
+
+def check_password(stored_password, provided_password):
+    # Convert the provided password to bytes
+    provided_password_bytes = provided_password.encode('utf-8')
+
+    # Check if the provided password matches the stored hashed password
+    return bcrypt.checkpw(provided_password_bytes, stored_password)
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -10,10 +28,12 @@ class User(UserMixin, db.Model):
     image_path = db.Column(db.String(256))  # Field to store the image path
     
     def set_password(self, password):
-        self.password = generate_password_hash(password, method='sha256')
+        # self.password = generate_password_hash(password, method='sha256')
+        self.password = hash_password(password)
         
     def check_password(self, password):
-        return check_password_hash(self.password, password)
+        return check_password(self.password, password)
+        # return check_password_hash(self.password, password)
     
     def set_img_path(self, path):
         self.image_path = path
